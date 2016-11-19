5b597794a566db3cc754eac6d583678d73c085e8
Guilherme Castelao <guilherme@castelao.net>
2016-11-19 12:40:46 -0800

    Basic download_file working.

--- forklift/forklift.py
+++ forklift/forklift.py
@@ -1,31 +1,44 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import shutil
+from tempfile import NamedTemporaryFile
+if sys.version_info >= (3, 0):
+    from urllib.request import urlopen
+    from urllib.parse import urlparse
+else:
+    from urllib2 import urlopen
+    from urlparse import urlparse
 
-def download_file(url, md5hash, d):
-    """ Download data file from web
+
+def download_file(outputdir, url, filename, md5hash=None):
+    """ Download data file from a URL
 
         IMPROVE it to automatically extract gz files
     """
+    assert os.path.exists(outputdir)
+
     download_block_size = 2 ** 16
 
-    assert type(md5hash) is str
+    #assert type(md5hash) is str
 
-    if not os.path.exists(d):
-        os.makedirs(d)
+    #hash = hashlib.md5()
 
-    hash = hashlib.md5()
+    src = os.path.join(url, filename)
+    fname = os.path.join(outputdir, filename)
 
-    fname = os.path.join(d, os.path.basename(urlparse(url).path))
-    if os.path.isfile(fname):
-        h = hashlib.md5(open(fname, 'rb').read()).hexdigest()
-        if h == md5hash:
-            print("Was previously downloaded: %s" % fname)
-            return
-        else:
-            assert False, "%s already exist but doesn't match the hash: %s" % \
-                    (fname, md5hash)
+    #fname = os.path.join(outputdir, os.path.basename(urlparse(url).path))
+    #if os.path.isfile(fname):
+    #    h = hashlib.md5(open(fname, 'rb').read()).hexdigest()
+    #    if h == md5hash:
+    #        print("Was previously downloaded: %s" % fname)
+    #        return
+    #    else:
+    #        assert False, "%s already exist but doesn't match the hash: %s" % \
+    #                (fname, md5hash)
 
-    remote = urlopen(url)
+    remote = urlopen(src)
 
     with NamedTemporaryFile(delete=False) as f:
         try:
@@ -33,7 +46,7 @@ def download_file(url, md5hash, d):
             block = remote.read(download_block_size)
             while block:
                 f.write(block)
-                hash.update(block)
+                #hash.update(block)
                 bytes_read += len(block)
                 block = remote.read(download_block_size)
         except:
@@ -41,12 +54,12 @@ def download_file(url, md5hash, d):
                 os.remove(f.name)
                 raise
 
-    h = hash.hexdigest()
-    if h != md5hash:
-        os.remove(f.name)
-        print("Downloaded file doesn't match. %s" % h)
-        assert False, "Downloaded file (%s) doesn't match with expected hash (%s)" % \
-                (fname, md5hash)
+    #h = hash.hexdigest()
+    #if h != md5hash:
+    #    os.remove(f.name)
+    #    print("Downloaded file doesn't match. %s" % h)
+    #    assert False, "Downloaded file (%s) doesn't match with expected hash (%s)" % \
+    #            (fname, md5hash)
 
     shutil.move(f.name, fname)
     print("Downloaded: %s" % fname)
